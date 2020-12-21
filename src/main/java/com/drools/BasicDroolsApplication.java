303b257057364e3a086c313d3f52b91b49e28be1
michel.santos <michel.santos@lead.fortaleza>
2020-12-21 10:08:56 -0300

    Create structure

---
+++ src/main/java/com/drools/BasicDroolsApplication.java
@@ -0,0 +1,13 @@
+package com.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicDroolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drools/BasicDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
